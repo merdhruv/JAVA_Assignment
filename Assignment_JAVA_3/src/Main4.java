7ff27240a76c776b107e86594f2a1d06c0cb7d13
Dhruv Pratap Singh <dhruvpratapsinghmer@gmail.com>
2023-10-16 21:20:52 +0530

    Assigment3Added

--- Assignment_JAVA_3/src/Main4.java
+++ Assignment_JAVA_3/src/Main4.java
@@ -2,18 +2,75 @@
 class Author{
 	private String name;
 	private String email;
-	private String gender;
+	private char gender;
 	
-	Author(String name, String email, String gender){
+	Author(String name, String email, char gender){
 		this.name = name;
 		this.email = email;
 		this.gender = gender;
 	}
+	String getName() {
+		return name;
+	}
+	String getEmail() {
+		return email;
+	}
+	void setEmail(String email) {
+		this.email = email;
+	}
+	char getGender() {
+		return gender;
+	}
+	public String toString() {
+		return name + " " + gender + " at "+ email;
+	}
+}
+
+class Book {
+	
+	private String name;
+	private Author author;
+	private double price;
+	private int quantity;
+
+	Book(String name, Author author, double price, int quantity) {
+		this.name = name;
+		this.author = author;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	String getName() {
+		return name;
+	}
+	Author getAuthor() {
+		return author;
+	}
+	double getPrice() {
+		return price;
+	}
+	int getQty() {
+		return quantity;
+	}
+	void setPrice(double price) {
+		this.price = price;
+	}
+	void setOty(int q) {
+		this.quantity = q;
+	}
+	public String toString() {
+		return name + " by "+ this.author.getName() + " " + this.author.getGender() + " at "+ this.author.getEmail();
+	}
+
 }
 
 public class Main4 {
 
 	public static void main(String[] args) {
+		Author a = new Author("James Clear",  "jamesclear@gmail.com", 'M');
+//		System.out.println(a.toString());
+		
+		Book b = new Book("Atomic Habits", a, 400, 3);
+		System.out.println(b.toString());
 		
 	}
 
---
+++ Assignment_JAVA_3/src/Main5.java
@@ -0,0 +1,103 @@
+
+class Animal{
+	void makeSound() {
+		System.out.println("laksd");
+	}
+}
+class Dog extends Animal{
+	void makeSound() {
+		System.out.println("woof");
+	}
+}
+class Cat extends Animal{
+	void makeSound() {
+		System.out.println("meow");
+	}
+}
+
+//----------------------------------------------------------------------------------------------
+
+abstract class Car{
+	abstract void speed() ;
+}
+
+class Maruti extends Car{
+	void speed() {
+		System.out.println("60kmph");
+	}
+}
+class Alto extends Car{
+	void speed() {
+		System.out.println("70kmph");
+	}
+}
+class Brezza extends Car{
+	void speed() {
+		System.out.println("80kmph");
+	}
+}
+//---------------------------------------------------------------------------------------------
+
+abstract class Shape{
+	abstract void draw();
+}
+
+class Triangle extends Shape{
+	void draw() {
+		System.out.println("Triangle Drawn");
+	}
+}
+
+class Rectangle extends Shape{
+	void draw() {
+		System.out.println("Rectangle Drawn");
+	}
+}
+
+class Circle extends Shape{
+	void draw() {
+		System.out.println("Circle Drawn");
+	}
+}
+
+//--------------------------------------------------------------------------------------------
+
+
+
+//---------------------------------------------------------------------------------------------
+
+public class Main5 {
+
+	public static void main(String[] args) {
+//		Animal a = new Animal();
+//		Dog d = new Dog();
+//		Cat c = new Cat();
+//		
+//		a.makeSound();
+//		d.makeSound();
+//		c.makeSound();
+		
+//----------------------------------------------------------------------------------------------
+//		Maruti m = new Maruti();
+//		Alto a = new Alto();
+//		Brezza b = new Brezza();
+//		
+//		m.speed();
+//		a.speed();
+//		b.speed();
+		
+//-----------------------------------------------------------------------------------------------
+//		Triangle t = new Triangle();
+//		Rectangle r = new Rectangle();
+//		Circle c = new Circle();
+//		
+//		t.draw();
+//		r.draw();
+//		c.draw();
+		
+//-----------------------------------------------------------------------------------------------
+		
+		
+		
+	}
+}
